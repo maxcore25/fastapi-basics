c1bf4871ac7707b0e4945692ea81978398f8e8fa
maxcore25 <mkorobov2002@mail.ru>
2022-01-09 21:07:52 +0300

    Fetch POST method added

--- script.js
+++ script.js
@@ -28,3 +28,16 @@ fetch('http://127.0.0.1:8000/items/3?q=any')
         divConsole.innerHTML = JSON.stringify(apiData);
     })
 
+
+fetch('http://127.0.0.1:8000', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({name: 'User 1'})
+})
+    .then(res => {
+        return res.json()
+    })
+    .then(data => console.log(data))
+    .then(error => console.log(error))
