9404b692625273926683bfd995e9466a3d1ce57a
maxcore25 <mkorobov2002@mail.ru>
2022-01-16 06:18:08 +0300

    Fetch output stylized a bit

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 let apiData = null
-const divConsole = document.querySelector('.console')
+const divConsole = document.querySelector('.console-main-frame')
 
-// fetch('http://127.0.0.1:8000/items/3?q=any')
-//     .then(res => res.json())
-//     .then(data => {
-//         console.log(data)
-//         apiData = data
-//     })
-//     .then(res => {
-//         // divConsole.innerHTML += apiData
-//     })
-//     .then(res => {
-//         let printObj = function (obj) {
-//             let string = '';
-//
-//             for (let prop in obj) {
-//                 if (typeof obj[prop] == 'string') {
-//                     string += prop + ': ' + obj[prop] + '; </br>';
-//                 } else {
-//                     string += prop + ': { </br>' + obj[prop] + '}';
-//                 }
-//             }
-//
-//             return string;
-//         }
-//         // divConsole.innerHTML += printObj(apiData)
-//         divConsole.innerHTML = JSON.stringify(apiData);
-//     })
-//     .catch(error => console.log('Fetch1: ' + error))
+fetch('http://127.0.0.1:8000/items/3?q=any')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        apiData = data
+    })
+    .then(res => {
+        // divConsole.innerHTML += apiData
+    })
+    .then(res => {
+        let printObj = function (obj) {
+            let string = '';
+
+            for (let prop in obj) {
+                if (typeof obj[prop] == 'string') {
+                    string += prop + ': ' + obj[prop] + '; </br>';
+                } else {
+                    string += prop + ': { </br>' + obj[prop] + '}';
+                }
+            }
+
+            return string;
+        }
+        // divConsole.innerHTML += printObj(apiData)
+        divConsole.innerHTML += JSON.stringify(apiData);
+    })
+    .catch(error => console.log('Fetch1: ' + error))
 
 
 // fetch('http://127.0.0.1:8000', {
